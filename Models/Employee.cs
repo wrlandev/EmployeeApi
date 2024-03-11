f6f40bbabe97fa8de913811601e1b121c5da51a0
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-03-11 18:24:30 -0300

    feat: create employee model

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Photo { get; set; }
+    }
+}
