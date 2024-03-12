967d4623dd36311b1470e6a5838d0f4a1f9c3587
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-03-12 18:53:13 -0300

    feat: create auth route

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/auth")]
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        public IActionResult Auth(string username, string password)
+        {
+            if (username == "filipe" && password == "123456")
+            {
+                var token = TokenService.GenerateToken(new Models.EmployeeModel());
+                return Ok(token);
+            }
+
+            return BadRequest("username or password invalid");
+        }
+    }
+}
