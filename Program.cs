bc64572a5f2b8a191fae4755d1e1fcdb4923cfeb
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-03-12 20:51:10 -0300

    feat: apply auto mapper

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using EmployeeApi.Domain.Models;
 using EmployeeApi.Infrastructure;
 using EmployeeApi.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -17,7 +16,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddAutoMapper(typeof(DomainToDtoMapping));
 builder.Services.AddSwaggerGen(c =>
 {
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
