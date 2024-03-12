5bf0d249ccf6b2f77f461cd685ebba0720397ede
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-03-12 18:43:26 -0300

    feat: create token validation

--- Key.cs
+++ Key.cs
@@ -2,5 +2,6 @@
 {
     public class Key
     {
+        public static string Secret = "123A123a123S123s123D123d";
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
+using EmployeeApi;
 using EmployeeApi.Infrastructure;
 using EmployeeApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,23 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddDbContext<AppDbContext>(Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
 
 var app = builder.Build();
 
